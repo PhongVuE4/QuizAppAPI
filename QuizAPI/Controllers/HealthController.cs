82e2dfeb252819773ca4bf8a711e140480fb4402
Phong Vu <phongvue4@gmail.com>
2025-10-16 09:20:18 +0700

    upd

---
+++ QuizAPI/Controllers/HealthController.cs
@@ -0,0 +1,21 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Quiz_Common;
+
+namespace QuizAPI.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class HealthController : ControllerBase
+    {
+        [HttpGet]
+        public async Task<IActionResult> GetHealthStatus()
+        {
+            return Ok(new
+            {
+                status = "Healthy",
+                timestamp = TimeHelper.GetVietnamCurrentTime()
+            });
+        }
+    }
+}
